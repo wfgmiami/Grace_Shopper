8b47540e97c0a2dd13e1b0ab36893e176997a46e
lucas1richard <=>
2017-04-20 12:47:13 -0400

    feed linter and make routes a folder

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const router = require('./routes');
-const bodyParser = require('body-parser');
-const db = require('../db')
-const path = require('path');
+const router = require( './routes' );
+const bodyParser = require( 'body-parser' );
+const db = require( '../db' );
+const path = require( 'path' );
 
-app.use(bodyParser.json())
-app.use('/vendor', express.static(path.join(__dirname,'..','node_modules')));
-app.use('/dist', express.static(path.join(__dirname,'..','dist')));
-app.use('/api', router);
+app.use( bodyParser.json() );
+app.use( '/vendor', express.static( path.join( __dirname, '..', 'node_modules' ) ) );
+app.use( '/dist', express.static( path.join( __dirname, '..', 'dist' ) ) );
+app.use( '/api', router );
 
 
-app.get('/',(req,res,next)=>{
-  res.sendFile(path.join(__dirname, '..','browser/index.html'));
-})
+app.get( '*', ( req, res, next ) => {
+  res.sendFile( path.join( __dirname, '..', 'browser/index.html' ) );
+} );
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> `listening on port ${port}`)
+app.listen( port, () => console.log(`Listening on port ${port}`) );
 
 //db.seed();
+
