0096f7384c1fbbe756cd7af7b6c7954ae9875362
lucas1richard <=>
2017-05-02 21:25:22 -0400

    interface update

--- browser/react/CategoryList/ProductListItem.js
+++ browser/react/CategoryList/ProductListItem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const ProductListItem = ({ product, addToCart, removeFromCart }) => (
-  <div className="col-xs-4">
-    <div style={{ maxwidth: '300px', margin: 'auto', marginBottom: '10%' }}>
-      <img style={{ marginBottom: '0' }} className="img-responsive" src={ product.images[0] } />
-      <div id = 'product details'> 
-        <div id = 'product name' style={{maxwidth: '80%'}}>
-        { product.name } 
-        </div>
+  <div className="col-lg-4 col-md-6 col-sm-12">
+    <div className="panel panel-default" style={{ width: '100%', margin: 'auto', marginBottom: '10%' }}>
+      <div className="panel-body">
         <span className="pull-right">
-          <span style={{ paddingRight: '15px' }}>
-            ${ product.price }
+          <span>
+            <button className="btn btn-default btn-xs" onClick={ () => addToCart( Object.assign( product, { lineitems: { quantity: 1 } } ) ) }>
+              ${ product.price + ' ' }
+              <span className="glyphicon glyphicon-shopping-cart" />
+            </button>
           </span>
-          <button className="glyphicon glyphicon-shopping-cart pull-right" onClick={ () => addToCart( Object.assign( product, { lineitems: { quantity: 1 } } ) ) } />
         </span>
+        <img style={{ width: '100%', marginBottom: '0' }} className="img-responsive" src={ product.images[0] } />
+      </div>
+      <div className="panel-footer"> <b>{ product.name }</b>
       </div>
     </div>
   </div>
